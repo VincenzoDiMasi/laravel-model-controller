88bf9f970b4c9b98acaaaf7d8df325d5994bf6d8
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-24 17:37:26 +0100

    Add some a little of style

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,10 +11,10 @@
 <body>
     <section id="movies">
         <div class="container">
-           <h1 class="my-3">home</h1>
+           <h1 class="my-3 text-center">Home</h1>
            <div class="d-flex flex-wrap">
                @forelse ($movies as $movie)
-               <div class="card" style="width: 18rem;">
+               <div class="card m-2" style="width: 18rem;">
                    <div class="card-body">
                      <h5 class="card-title">{{$movie->title}}</h5>
                      <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
