639b3c53a0ac2530cc64fb4e59963559aae6b565
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-24 17:33:31 +0100

    Try to print

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,26 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <h1 class="text-center">AAAAAAA</h1>
+    <section id="movies">
+        <div class="container">
+           <h1 class="my-3">home</h1>
+           <div class="d-flex flex-wrap">
+               @forelse ($movies as $movie)
+               <div class="card" style="width: 18rem;">
+                   <div class="card-body">
+                     <h5 class="card-title">{{$movie->title}}</h5>
+                     <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
+                     <p class="card-text">{{$movie->nationality}}</p>
+                     <p class="card-text">{{$movie->vote}}</p>
+                     <p class="card-text">{{$movie->date}}</p>
+                   </div>
+                 </div>
+                   
+               @empty
+                   <h1>tutt appost</h1>
+               @endforelse
+           </div>
+        </div>
+       </section>
 </body>
 </html>
